eaee85e49575f2839d5fcce91c225e9166c448bb
hoannc0506 <hoan.nc0506@gmail.com>
2024-08-16 15:55:29 +0700

    Update basic rag pipeline

---
+++ rag_basic_pipeline.py
@@ -0,0 +1,54 @@
+import logging 
+import sys 
+import nest_asyncio
+from llama_index.core import (
+    StorageContext, Settings, VectorStoreIndex
+)
+import model_utils, agent_utils, index_utils
+
+# async config
+nest_asyncio.apply()
+
+# logging config
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout)) 
+
+def main():
+    device_map = "cuda:0"
+    # load llm and embedding
+    llm_hf, embed_model = agent_utils.load_llm_embed_models(
+        llm_name="models/Meta-Llama-3.1-8B-Instruct", 
+        embed_name="models/bge-base-en-v1.5", 
+        device_map=device_map
+    )
+
+    # Update llamaindex settings
+    Settings.llm = llm_hf
+    Settings.embed_model = embed_model
+        
+    # indexing
+    vector_index = index_utils.creat_qdrant_index()
+    
+    # querying
+    query_engine = vector_index.as_query_engine(
+        response_mode="compact",
+        streaming=True
+    )
+
+    # sample questions
+    questions = [
+    "What are key features of llama-agents?",
+    '''What are the two critical areas of RAG system performance that are \
+assessed in the "Evaluating RAG with LlamaIndex" section of the OpenAI Cookbook?'''
+]
+    for idx, question in enumerate(questions):
+        print("Question:", question)
+        response = query_engine.query(question)
+        response.print_response_stream()
+        # print_ref_docs(response.metadata)
+        print("\n=="*40)
+    
+
+if __name__ == "__main__":
+    import fire
+    fire.Fire(main)
